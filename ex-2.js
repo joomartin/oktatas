d3d6cd8afd9431e7a4b767bd67616035eeb3bcb6
joomartin <martin.joo@prosupport.io>
2018-01-03 12:38:35 +0100

    ex3

--- ex-2.js
+++ ex-2.js
@@ -8,3 +8,22 @@ const persons = [
     { gender: 'male', name: 'dsfgds133' }
 ];
 
+const result = {
+    male: 0,
+    female: 0
+};
+
+for (const p of persons) {
+    if (p.gender === 'male') result.male++;
+    else result.female++;
+}
+
+const r = persons
+    .reduce((acc, p) => (acc[p.gender]++, acc), { male: 0, female: 0 });
+
+console.log(r);
+// console.log(result);
+
+// olyan objektumot aminek két indexe
+// male: number (hány darab férfi van a tömbben)
+// female: number (hány darab nő van a tömbben)
---
+++ ex-3.js
@@ -0,0 +1,12 @@
+const arr = [
+    [1,2,3],    // 0
+    [4,5,6],    // 1
+    [7,8,9]     // 2
+];
+
+// új array -t, aminek minden indexében
+// az 'arr' átlaga szerepel
+
+/**
+ * [2, 5, 8]
+ */
