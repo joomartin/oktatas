9d1dd3db7516b96947f411c5bae512002f10c6f3
Unknown <bendr424@gmail.com>
2018-01-25 10:35:47 +0100

    users

--- jan25/git_basics/app.js
+++ jan25/git_basics/app.js
@@ -2,7 +2,8 @@ const USERS = [
     { id: 1, name: 'Martin' },
     { id: 2, name: 'Bende' },
     { id: 3, name: 'Olivér' },
-    { id: 4, name: 'Pisti' }
+    { id: 4, name: 'Pisti' },
+    { id: 5, name: 'Marci' }
 ];  
 
 console.log('yayyy');
