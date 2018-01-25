8d336d0bddad44f37a752314b23d8c0ac1bca515
Joó Martin <m4rt1n.j00@gmail.com>
2018-01-25 10:36:18 +0100

    béla

--- jan25/git_basics/app.js
+++ jan25/git_basics/app.js
@@ -2,7 +2,8 @@ const USERS = [
     { id: 1, name: 'Martin' },
     { id: 2, name: 'Bende' },
     { id: 3, name: 'Olivér' },
-    { id: 4, name: 'Pisti' }
+    { id: 4, name: 'Pisti' },
+    { id: 6, name: 'Béla' }
 ];  
 
 console.log('yayyy');
