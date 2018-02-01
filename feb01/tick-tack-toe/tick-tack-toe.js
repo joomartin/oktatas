6b3fdf5d75727bdaf27b056b7254cf76cb6287ee
Joó Martin <m4rt1n.j00@gmail.com>
2018-02-01 13:34:51 +0100

    ticktock

--- feb01/tick-tack-toe/tick-tack-toe.js
+++ feb01/tick-tack-toe/tick-tack-toe.js
@@ -1,21 +1,29 @@
 let currentTurn = 'X';
+let isGameOver = false;
 
 $('table td').click(function () {
+    if (isGameOver) {
+        return;
+    }
+
     const td = $(this);
     const tr = td.parent();
 
     const trIndex = $('table tr').index(tr);
     const tdIndex = tr.find('td').index(td);
 
-    if (td.text() !== '') {
+    if (td.text() !== '')
         return;
-    }
 
     td.text(currentTurn);
-    td.addClass(getTurnClassName());      
+    td.addClass(getTurnClassName());
 
     const matrix = tableToMatrix();
     const points = getPoints(matrix, trIndex, tdIndex);
+
+    if (isDraw(matrix)) {
+        alert('Dontetlen');
+    }
 
     flipTurn();
 });
@@ -55,11 +63,27 @@ function getPoints(matrix, rows, columns) {
     const rightPoints = right(matrix, rows, columns);
     const leftPoints = left(matrix, rows, columns);
 
-    if (leftPoints + rightPoints === 4)
-        alert('Nyertél (viz)');
+    const rightUpPoints = rightUp(matrix, rows, columns);
+    const leftDownPoints = leftDown(matrix, rows, columns);
 
-    if (upPoints + downPoints === 4)
-        alert('Nyertél (fuggo)');
+    const leftUpPoints = leftUp(matrix, rows, columns);
+    const rightDownPoints = rightDown(matrix, rows, columns);
+
+    if (leftPoints + rightPoints === 4) {
+        gameOver();
+    }
+
+    if (upPoints + downPoints === 4) {
+        gameOver();
+    }
+
+    if (rightUpPoints + leftDownPoints === 4) {
+        gameOver();
+    }
+
+    if (leftUpPoints + rightDownPoints === 4) {
+        gameOver();
+    }
 }
 
 function up(matrix, rows, columns) {
@@ -73,7 +97,7 @@ function up(matrix, rows, columns) {
     }
 
     return points;
-}   
+}
 
 function down(matrix, rows, columns) {
     let points = 0;
@@ -86,7 +110,7 @@ function down(matrix, rows, columns) {
     }
 
     return points;
-}  
+}
 
 function right(matrix, rows, columns) {
     let points = 0;
@@ -99,7 +123,7 @@ function right(matrix, rows, columns) {
     }
 
     return points;
-} 
+}
 
 function left(matrix, rows, columns) {
     let points = 0;
@@ -112,4 +136,81 @@ function left(matrix, rows, columns) {
     }
 
     return points;
-} 
+}
+
+function rightUp(matrix, rows, columns) {
+    let points = 0;
+    let j = columns + 1;
+
+    for (let i = rows - 1; i >= 0; i--) {
+        if (matrix[i][j++] === currentTurn) {
+            points++;
+        } else {
+            return points;
+        }
+    }
+
+    return points;
+}
+
+function leftDown(matrix, rows, columns) {
+    let points = 0;
+    let j = columns - 1;
+
+    for (let i = rows + 1; i < matrix.length; i++) {
+        if (matrix[i][j--] === currentTurn) {
+            points++;
+        } else {
+            return points;
+        }
+    }
+
+    return points;
+}
+
+function leftUp(matrix, rows, columns) {
+    let points = 0;
+    let j = columns - 1;
+
+    for (let i = rows - 1; i >= 0; i--) {
+        if (matrix[i][j--] === currentTurn) {
+            points++;
+        } else {
+            return points;
+        }
+    }
+
+    return points;
+}
+
+function rightDown(matrix, rows, columns) {
+    let points = 0;
+    let j = columns + 1;
+
+    for (let i = rows + 1; i < matrix.length; i++) {
+        if (matrix[i][j++] === currentTurn) {
+            points++;
+        } else {
+            return points;
+        }
+    }
+
+    return points;
+}
+
+function gameOver() {
+    isGameOver = true;
+    setTimeout(() => {
+        const confirmValue = confirm(`${currentTurn} nyert! Akarsz ujra jatszani?`);
+
+        if (confirmValue) {
+            window.location.reload();
+        }
+    }, 0);
+}
+
+function isDraw(matrix) {
+    return !isGameOver && matrix
+        .map(arr => arr.some(item => item === ''))
+        .every(item => !item);
+}
