162e61975686808be0d75cbb1fac6a4aac69cc99
Joó Martin <m4rt1n.j00@gmail.com>
2018-02-01 10:17:42 +0100

    tick tack

--- feb01/tick-tack-toe/tick-tack-toe.js
+++ feb01/tick-tack-toe/tick-tack-toe.js
@@ -3,21 +3,18 @@ let currentTurn = 'X';
 $('table td').click(function () {
     const td = $(this);
 
-    if (td.text() === '') {
-        td.text(currentTurn);
-        
-        td.addClass(getTurnClassName());      
-
-        flipTurn();
+    if (td.text() !== '') {
+        return;
     }
+
+    td.text(currentTurn);
+    td.addClass(getTurnClassName());      
+
+    flipTurn();
 });
 
 function flipTurn() {
-    if (currentTurn === 'X') {
-        currentTurn = 'O';
-    } else {
-        currentTurn = 'X';
-    }
+    currentTurn = (currentTurn === 'X') ? 'O' : 'X';
 
     $('#current-turn')
         .text(currentTurn)
