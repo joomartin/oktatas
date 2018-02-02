95a7b95f73f68b68e6c3d2791289d7c5819acb86
Joó Martin <m4rt1n.j00@gmail.com>
2018-02-02 10:59:27 +0100

    do nothing

---
+++ feb02/do-nothing/app.js
@@ -0,0 +1,54 @@
+const counter = $('#counter');
+const error = $('h1.error');
+
+let date;
+let interval;
+let isOver = false;
+
+resetDate();
+setCounter(date);
+
+interval = setInterval(function () {
+    if (date.format('mm:ss') === '00:00') {
+        clearInterval(interval);
+        alert('Nyertel!!!');
+        isOver = true;
+    } else {
+        date = moment(date).subtract(1, 's');
+        setCounter(date);
+    }
+}, 1000);
+
+$(document).click(function () {
+    eventHappened();
+});
+
+$(document).keypress(function () {
+    eventHappened();
+});
+
+$(document).mousemove(function () {
+    eventHappened();
+});
+
+function eventHappened() {
+    if (isOver) {
+        return;
+    }
+
+    error.show().fadeOut(2000);
+    resetCounter();
+}
+
+function resetCounter() {
+    resetDate();
+    setCounter(date);
+}
+
+function setCounter(date) {
+    counter.text(date.format('mm:ss'));
+}
+
+function resetDate() {
+    date = moment(2, 'm');
+}
