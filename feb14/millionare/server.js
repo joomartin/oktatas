e5b1eeaf503f0d9a66cdd16471edb454ec46b028
Joó Martin <m4rt1n.j00@gmail.com>
2018-02-14 11:28:29 +0100

    server végpontok

--- feb14/millionare/server.js
+++ feb14/millionare/server.js
@@ -1,12 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('promise-mysql');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello World');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8000, () => {
-    console.log('Server listening on port 8000');
-});
+(async () => {
+
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        port: '8889',   // <- EZ ALAPBÓL NEM KELL
+        user: 'root',
+        password: 'root',
+        database: 'millionare'
+    });
+
+    app.get('/questions', async (request, response) => {
+        const questions = await conn.query('SELECT * FROM questions');
+
+        response.json(questions);
+    });
+
+    app.get('/questions/random', async (request, response) => {
+        const questions = await conn.query('SELECT * FROM questions WHERE difficulty = ' + request.query.difficulty);
+        const result = questions.sort(() => Math.random() - 0.5);
+
+        const question = result[0];
+        const answers = await conn.query(`SELECT * FROM answers WHERE question_id = ${question.id}`);
+
+        question.answers = answers;
+
+        response.json(question);
+    });
+
+    app.get('/questions/isCorrect', async (request, response) => {
+        const result = await conn.query(`SELECT * FROM questions WHERE id = ${request.query.questionId} LIMIT 1`);
+        const question = result[0];
+
+        if (question.correctAnswer == request.query.answer) {
+            response.json({
+                isCorrect: true,
+                correctAnswer: question.correctAnswer
+            });
+        } else {
+            response.json({
+                isCorrect: false,
+                correctAnswer: question.correctAnswer
+            });
+        }
+
+        response.json(questions);
+    });
+    
+    app.listen(8000, () => {
+        console.log('Server listening on port 8000');
+    });
+})();
