25140209d30a6d12dc3721e784e3e8f226d6ac63
Joó Martin <m4rt1n.j00@gmail.com>
2018-02-22 09:22:14 +0100

    poly

---
+++ feb14/millionare/poly.js
@@ -0,0 +1,37 @@
+function evaluateAnswer(answer) {
+    if (answer === 'A') {
+        console.log('szipi szupi');
+    } else {
+        console.log('nem jo');
+    }
+}
+
+const answer = createAnswer('A');
+answer.evaluate();
+
+
+function createAnswer(letter) {
+    if (letter === 'A') {
+        return new CorrectAnswer;
+    }
+
+    return new IncorrectAnswer;
+}
+
+class Answer {
+    evaluate() {
+
+    }
+}
+
+class CorrectAnswer extends Answer {
+    evaluate() {
+        console.log('szipi szupi');
+    }
+}
+
+class IncorrectAnswer extends Answer {
+    evaluate() {
+        console.log('nem jo');
+    }
+}
